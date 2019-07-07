41be32c9ffb955b2d46a5469a43fec14c4727a57
Flakebi <flakebi@t-online.de>
2019-07-07 16:31:24 +0200

    Use wee allocator for wasm
    Also compile with optimizations in debug mode, otherwise firefox will
    complain about too many used registers.

--- proxy/src/main.rs
+++ proxy/src/main.rs
@@ -542,7 +542,7 @@ fn main() -> Result<(), Error> {
 				HttpResponse::Ok()
 			});
 		}
-		app = app.handler("/", StaticFiles::new("../target/wasm32-unknown-unknown/release")
+		app = app.handler("/", StaticFiles::new("../target/wasm32-unknown-unknown/debug")
 			.expect("static files not found")
 			.default_handler(StaticFiles::new("../frontend/static/")
 				.expect("Static files not found")
