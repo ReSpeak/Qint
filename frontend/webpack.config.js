e5efbd39187c8e89e25d64dd40af84e1d37795cd
Till <asuro@posteo.de>
2020-02-09 22:10:33 +0100

    Webpack dev server

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -4,22 +4,39 @@ const webpack = require('webpack');
 const WasmPackPlugin = require("@wasm-tool/wasm-pack-plugin");
 
 module.exports = {
-	entry: './index.js',
-	output: {
-		path: path.resolve(__dirname, 'dist'),
-		filename: 'index.js',
-	},
-	plugins: [
-		new HtmlWebpackPlugin(),
-		new WasmPackPlugin({
-			crateDirectory: path.resolve(__dirname, ".")
-		}),
-		// Have this example work in Edge which doesn't ship `TextEncoder` or
-		// `TextDecoder` at this time.
-		new webpack.ProvidePlugin({
-		  TextDecoder: ['text-encoding', 'TextDecoder'],
-		  TextEncoder: ['text-encoding', 'TextEncoder']
-		})
-	],
-	mode: 'development'
+    entry: './index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'index.js',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "Qint",
+            template: "static/index.html"
+        }),
+        new WasmPackPlugin({
+            crateDirectory: path.resolve(__dirname, ".")
+        }),
+        // Have this example work in Edge which doesn't ship `TextEncoder` or
+        // `TextDecoder` at this time.
+        new webpack.ProvidePlugin({
+            TextDecoder: ['text-encoding', 'TextDecoder'],
+            TextEncoder: ['text-encoding', 'TextEncoder']
+        })
+    ],
+    mode: 'development',
+    devServer: {
+        proxy: [
+            {
+                context: ['/list', '/audiosend', '/plugins', '/bookmarks', '/messages'],
+                target: 'http://localhost:4422'
+            },
+            {
+                context: '/con',
+                target: 'http://localhost:4422',
+                ws: true
+            }
+        ],
+        contentBase: path.join(__dirname, 'static')
+    }
 };
