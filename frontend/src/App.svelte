94d7920ec016faa69e0f1cb3abd7ac6604d61491
Splamy <splamyn@gmail.com>
2020-07-17 04:13:50 +0200

    Add tab close confirmation
    Closes #5

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,5 +1,6 @@
 <script>
 	import { onMount, onDestroy } from "svelte";
+	import { get } from "svelte/store";
 	import { writable } from "svelte/store";
 	import Connect from "./connect/Connect.svelte";
 	import Connected from "./Connected.svelte";
@@ -7,6 +8,16 @@
 
 	export let connection;
 	$: state = connection.state;
+
+	window.onbeforeunload = function(e) {
+		let s = get(state);
+		if (s === ConnectionState.Connected || s === ConnectionState.ChannelListFinished) {
+			if (e) {
+				e.returnValue = true;
+			}
+			return true;
+		}
+	};
 </script>
 
 {#if $state !== ConnectionState.Connected && $state !== ConnectionState.ChannelListFinished}
@@ -31,7 +42,6 @@
 		background-color: $background;
 	}
 
-
 	.hover.menu {
 		position: fixed;
 		z-index: 3;
@@ -54,8 +64,8 @@
 	}
 
 	:global(.update-flash) {
-		color: rgba(255,62,0,1) !important;
-		background-color: rgba(255,62,0,0.2) !important;
+		color: rgba(255, 62, 0, 1) !important;
+		background-color: rgba(255, 62, 0, 0.2) !important;
 	}
 	:global(.update-flash-fade) {
 		transition: color 1s, background 1s;
