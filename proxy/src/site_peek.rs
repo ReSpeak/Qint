24bf5f02b98111a54f9d4606cc0f97b023c79311
Splamy <splamyn@gmail.com>
2020-12-06 02:27:41 +0100

    Better link analysis

--- proxy/src/site_peek.rs
+++ proxy/src/site_peek.rs
@@ -5,28 +5,21 @@ use std::sync::Mutex;
 use anyhow::{anyhow, Result};
 use percent_encoding::percent_decode_str;
 use reqwest::header::CONTENT_TYPE;
-use scraper::{Html, Selector};
-use serde::{Deserialize, Serialize};
+use scraper::{ElementRef, Html, Selector};
+use serde::Serialize;
 
 #[derive(Debug, Default)]
 pub struct SitePeekCache {
 	cache: Mutex<HashMap<String, AnalyzeResult>>,
 }
 
-#[derive(Clone, Debug, Serialize, Deserialize)]
+#[derive(Clone, Debug, Serialize)]
 pub enum AnalyzeResult {
 	Unknown,
 	// Image,Video link, currently same as input, but might change later
 	Image(String),
 	Video(String),
-	Site(AnalyzeResultSite),
-}
-
-#[derive(Clone, Debug, Serialize, Deserialize)]
-pub struct AnalyzeResultSite {
-	title: String,
-	image_src: String,
-	description: Option<String>,
+	Site { title: String, image_src: String, description: Option<String> },
 }
 
 impl SitePeekCache {
@@ -64,41 +57,78 @@ impl SitePeekCache {
 		} else if content_type.starts_with("text/html") {
 			let document_str = response.text().await?;
 			let document = Html::parse_document(&document_str);
-
-			// TODO also add reader for
-			// - <meta name="Description" content="Phoronix is the leading technology …">
-			// - <title>Open-Source RADV Vulkan Driver Is Seeing Work To … - Phoronix</title>
-			// - <link rel="icon" type="image/png" href="/android-chrome-192x192.png" sizes="192x192"> (pick biggest size)
-			let selector_title = Selector::parse("meta[property='og:title']").unwrap();
-			let selector_image = Selector::parse("meta[property='og:image']").unwrap();
-			let selector_descr = Selector::parse("meta[property='og:description']").unwrap();
-			let title = Selector::parse("title").unwrap();
-			let favicon = Selector::parse("link[rel='icon']").unwrap();
-			let meta_descr = Selector::parse("meta[name='description']").unwrap();
-
-			let title = document
-				.select(&selector_title)
+			let head = document
+				.select(&Selector::parse("head").unwrap())
 				.next()
-				.or_else(|| document.select(&title).next())
-				.and_then(|e| e.value().attr("content"))
-				.map(|e| e.to_string())
+				.ok_or_else(|| anyhow!("No head"))?;
+
+			// Special filtering for video-only content
+			if let Some(og_type) = Self::try_select(&head, "meta[property='og:type']", "content") {
+				if og_type.starts_with("video") {
+					if let Some(video_url) =
+						Self::try_select(&head, "meta[property='og:video']", "content")
+					{
+						return Ok(AnalyzeResult::Video(video_url.to_string()));
+					}
+				}
+			}
+
+			// Special filtering for curated sites
+			if link.starts_with("https://imgur.com/") {
+				if let Some(img_url) =
+					Self::try_select(&head, "meta[name='twitter:image']", "content")
+				{
+					return Ok(AnalyzeResult::Image(img_url.to_string()));
+				}
+			}
+
+			// General (OG) analysis
+			let title = Self::try_select(&head, "meta[property='og:title']", "content")
+				.or_else(|| {
+					head.select(&Selector::parse("title").unwrap())
+						.next()
+						.and_then(|e| e.text().next())
+				})
 				.ok_or_else(|| anyhow!("No title"))?;
-			let image_src = document
-				.select(&selector_image)
-				.next()
-				.or_else(|| document.select(&favicon).next())
-				.and_then(|e| e.value().attr("content"))
-				.map(|e| e.to_string())
+			let image_src = Self::try_select(&head, "meta[property='og:image']", "content")
+				.or_else(|| {
+					let favicon = Selector::parse("link[rel='icon']").unwrap();
+					document
+						.select(&favicon)
+						.filter_map(|elem| {
+							let elem = elem.value();
+							elem.attr("href").map(|link| {
+								(
+									link,
+									elem.attr("sizes")
+										.and_then(|sizes| {
+											sizes.find('x').and_then(|index| {
+												sizes[..index].parse::<u32>().ok()
+											})
+										})
+										.unwrap_or_default(),
+								)
+							})
+						})
+						.max_by_key(|(_, size)| *size)
+						.map(|(link, _)| link)
+				})
 				.ok_or_else(|| anyhow!("No image"))?;
-			let description = document
-				.select(&selector_descr)
-				.next()
-				.or_else(|| document.select(&meta_descr).next())
-				.and_then(|e| e.value().attr("content"))
-				.map(|e| e.to_string());
-			Ok(AnalyzeResult::Site(AnalyzeResultSite { title, image_src, description }))
+
+			let description = Self::try_select(&head, "meta[property='og:description']", "content")
+				.or_else(|| Self::try_select(&head, "meta[name='description']", "content"));
+			Ok(AnalyzeResult::Site {
+				title: title.to_string(),
+				image_src: image_src.to_string(),
+				description: description.map(|e| e.to_string()),
+			})
 		} else {
 			Ok(AnalyzeResult::Unknown)
 		}
+	}
+
+	fn try_select<'a>(head: &'a ElementRef, selector: &str, attr: &str) -> Option<&'a str> {
+		let selector = Selector::parse(selector).unwrap();
+		head.select(&selector).next().and_then(|e| e.value().attr(attr))
 	}
 }
